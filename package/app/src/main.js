a1ce1c8610f8860128453adfc4c1672b85e6f94f
Surma <surma@surma.dev>
2023-12-03 11:52:34 +0000

    First couple steps

--- package/app/src/main.js
+++ package/app/src/main.js
@@ -1,5 +1,2 @@
-import { shouldBe42 } from "@surma/vanilla_js";
-import { greet } from "@surma/ts";
-
-document.body.append(greet("Surma"));
-document.body.append(`The result is: ${shouldBe42()}`);
+import * as preact from "preact";
+console.log(process.version, typeof preact.Component);
--- package/vanilla_js/main.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash-es";
-
-export function shouldBe42() {
-	let a = [40, 2];
-	if (true) {
-		a.push(5);
-	}
-	return _.sum(a);
-}
