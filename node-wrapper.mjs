bd7487f832928617165f91308d5bd39dc523c481
Surma <surma@surma.dev>
2023-12-04 13:52:13 +0000

    Make the script work

--- node-wrapper.mjs
+++ node-wrapper.mjs
@@ -4,9 +4,8 @@ import { spawnSync } from "node:child_process";
 
 // Replace all `${name}` expressions in `str` with `vars[name]`.
 const VAR_REGEXP = /\$\{([^}]+)\}/g;
-function expandVariables(str, vars, { keepUnknown = false } = {}) {
-	return str.replace(VAR_REGEXP, (match, varName) => {
-		if (keepUnknown && !vars[varName]) return match;
+function expandVariables(str, vars) {
+	return str.replace(VAR_REGEXP, (_match, varName) => {
 		return vars[varName] ?? "";
 	});
 }
@@ -19,21 +18,30 @@ function expandEnvVariables(env) {
 	return env;
 }
 
-const { MARKER_OUT, PACKAGE_NAME } = process.env;
+// TODO: Rename MARKER_OUT to OUT_DIR_FILE or something
+const { MARKER_OUT, PACKAGE_NAME, EXECROOT = process.cwd() } = process.env;
 // Derive additional, helpful values.
-const EXECROOT = process.cwd();
-const PACKAGE_OUT_DIR = path.join(EXECROOT, path.dirname(MARKER_OUT));
-const PACKAGE_SRC_DIR = path.join(EXECROOT, PACKAGE_NAME);
+const PACKAGE_DIR = MARKER_OUT.startsWith("/")
+	? path.dirname(MARKER_OUT)
+	: path.join(EXECROOT, path.dirname(MARKER_OUT));
 const additionalEnv = {
 	EXECROOT,
-	PACKAGE_OUT_DIR,
-	PACKAGE_SRC_DIR,
-	BINDIR: PACKAGE_OUT_DIR.slice(0, -1 * PACKAGE_NAME.length),
+	PACKAGE_DIR,
+	BINDIR: PACKAGE_DIR.slice(0, -1 * PACKAGE_NAME.length),
 };
 // Expand all variables
 const newEnv = expandEnvVariables(
 	Object.assign({}, process.env, additionalEnv),
 );
+
+if (newEnv.SYMLINKS) {
+	for (const symlink of newEnv.SYMLINKS.split(";")) {
+		let [target, symlinkPath] = symlink
+			.split(":")
+			.map((p) => path.normalize(p));
+		fs.symlinkSync(target, symlinkPath);
+	}
+}
 
 const [cmd, ...args] = process.argv
 	.slice(2)
@@ -44,19 +52,5 @@ const result = spawnSync(cmd, args, {
 	cwd: newEnv.CHDIR,
 	env: newEnv,
 });
-
-fs.writeFileSync(
-	process.env.MARKER_OUT,
-	JSON.stringify(
-		{
-			env: process.env,
-			newEnv: newEnv,
-			cmd,
-			args,
-		},
-		null,
-		2,
-	),
-);
 
 process.exit(result.status);
--- package/app/src/main.js
+++ package/app/src/main.js
@@ -4,9 +4,13 @@ import * as preact from "preact";
 const args = process.argv.slice(2);
 
 fs.writeFileSync(
-  args[0],
-  JSON.stringify({
-    v: process.version, 
-    c: preact.Component.name
-  }, null, 2)
+	args[0],
+	JSON.stringify(
+		{
+			v: process.version,
+			c: preact.Component.name,
+		},
+		null,
+		2,
+	),
 );
