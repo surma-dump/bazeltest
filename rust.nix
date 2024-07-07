cd4bdcb7acc24756680f514033109cef4e08eeb9
Surma <surma@surma.dev>
2024-07-07 17:22:15 +0100

    Try with standard derivation

--- rust.nix
+++ rust.nix
@@ -1,37 +1,28 @@
 { 
-  # pkgs ? import <nixpkgs> {},
-  pkgs ? import /Users/surma/src/github.com/NixOS/nixpkgs {},
+  pkgs ? import <nixpkgs> {},
+  # pkgs ? import /Users/surma/src/github.com/NixOS/nixpkgs {},
 }:
 let 
-  target = "wasm32-unknown-unknown";
-  # target = "wasm32-wasi";
+  # target = "wasm32-unknown-unknown";
+  target = "wasm32-wasi";
   fenix = import ./fenix.nix {inherit pkgs;};
-  naersk-lib = import ./naersk.nix {inherit pkgs;};
   toolchain =  fenix.combine [
     fenix.stable.rustc
     fenix.stable.cargo
     (builtins.getAttr target fenix.targets).stable.rust-std
   ];
-  naersk = pkgs.callPackage naersk-lib {
-    rustc = toolchain;
-    cargo = toolchain;
-  };
-  rustPlatform = pkgs.makeRustPlatform {
-    rustc = toolchain;
-    cargo = toolchain;
-    # stdenv = pkgs.pkgsCross.wasi32.stdenv;
-  };
-in 
-# rustPlatform.buildRustPackage {
-#   name = "lol";
-#   cargoLock = {
-#     lockFile = ./Cargo.lock;
-#   };
-#   auditable = false;
-#   srcs = ./.;
-# }
-naersk.buildPackage {
-  name = "lol";
   src = ./.;
-  CARGO_BUILD_TARGET = target;
+in 
+derivation {
+  inherit src;
+  name = "lol";
+  system = builtins.currentSystem;
+  builder = "${pkgs.bash}/bin/bash";
+  args = ["-c" ''
+    export PATH=$PATH:${pkgs.coreutils}/bin:${toolchain}/bin
+    cp -r ${src}/. .
+    cargo build -r --target ${target}
+    mkdir -p $out/bin
+    cp target/${target}/release/lol.wasm $out/bin
+  ''];
 }
