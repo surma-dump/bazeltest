f73d0ca7737d14919730768976313fb15d328e94
Surma <surma@surma.dev>
2024-07-07 16:07:00 +0100

    Target

--- rust.nix
+++ rust.nix
@@ -1,22 +1,37 @@
 { 
-  pkgs ? import <nixpkgs> {},
-  # pkgs ? import /Users/surma/src/github.com/NixOS/nixpkgs {},
+  # pkgs ? import <nixpkgs> {},
+  pkgs ? import /Users/surma/src/github.com/NixOS/nixpkgs {},
 }:
 let 
+  target = "wasm32-unknown-unknown";
+  # target = "wasm32-wasi";
   fenix = import ./fenix.nix {inherit pkgs;};
   naersk-lib = import ./naersk.nix {inherit pkgs;};
   toolchain =  fenix.combine [
     fenix.stable.rustc
     fenix.stable.cargo
-    fenix.targets.wasm32-wasi.stable.rust-std
+    (builtins.getAttr target fenix.targets).stable.rust-std
   ];
   naersk = pkgs.callPackage naersk-lib {
     rustc = toolchain;
     cargo = toolchain;
   };
+  rustPlatform = pkgs.makeRustPlatform {
+    rustc = toolchain;
+    cargo = toolchain;
+    # stdenv = pkgs.pkgsCross.wasi32.stdenv;
+  };
 in 
+# rustPlatform.buildRustPackage {
+#   name = "lol";
+#   cargoLock = {
+#     lockFile = ./Cargo.lock;
+#   };
+#   auditable = false;
+#   srcs = ./.;
+# }
 naersk.buildPackage {
   name = "lol";
   src = ./.;
-  CARGO_BUILD_TARGET = "wasm32-wasi";
+  CARGO_BUILD_TARGET = target;
 }
