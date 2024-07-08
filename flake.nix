7b2c48e74f1fac7ad65aa1a7826710d45b10d560
Surma <surma@surma.dev>
2024-07-08 11:39:38 +0100

    Nixfmt

--- flake.nix
+++ flake.nix
@@ -14,30 +14,28 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix, naersk, flake-utils}: 
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        packages.default = 
-          let
-            target = "wasm32-unknown-unknown";
-            pkgs = import nixpkgs {inherit system;};
-            lib = pkgs.lib;
-            toolchain = with fenix.packages.${system}; combine [
-              stable.rustc
-              stable.cargo
-              targets.${target}.stable.rust-std
-            ];
-            naersk' = pkgs.callPackage naersk {
-              rustc = toolchain;
-              cargo = toolchain;
-            };
-          in 
-          naersk'.buildPackage {
-            name = "lol";
-            src = lib.cleanSource ./.;
-            release = true;
-            copyLibs = true;
-            CARGO_BUILD_TARGET = target;
-          };
+  outputs = { self, nixpkgs, fenix, naersk, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = let
+        target = "wasm32-unknown-unknown";
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        toolchain = with fenix.packages.${system};
+          combine [
+            stable.rustc
+            stable.cargo
+            targets.${target}.stable.rust-std
+          ];
+        naersk' = pkgs.callPackage naersk {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+      in naersk'.buildPackage {
+        name = "lol";
+        src = lib.cleanSource ./.;
+        release = true;
+        copyLibs = true;
+        CARGO_BUILD_TARGET = target;
+      };
     });
 }
