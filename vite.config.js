5b2e58fa91f049deffb8bbc63e450080395b38a8
Surma <surma@surma.dev>
2024-07-08 18:15:17 +0100

    Teach vite about nix deps

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,35 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+
+const pkg = JSON.parse(await fs.readFile(new URL("./package.json", import.meta.url).pathname, "utf8"));
+
 import { defineConfig } from "vite";
+
+async function isFile(p) {
+  return fs.stat(p).then(s => s.isFile()).catch(() => false);
+}
+
+async function gatherBuildInputs(inputs = process.env.buildInputs) {
+  const aliases = await Promise.all(
+    inputs.split(" ").map(async input => {
+      const depPkgPath = path.join(input, "lib", "node_modules", pkg.name);
+      const depPkgJson = path.join(depPkgPath, "package.json");
+      if (!(await isFile(depPkgJson))) return [];
+      const depPkg = JSON.parse(await fs.readFile(depPkgJson, "utf8"));
+      return [[depPkg.name, depPkgPath]];
+    }),
+  );
+  return Object.fromEntries(aliases.flat());
+}
 
 export default defineConfig({
   resolve: {
     alias: {
-      // This would be automatically set up by a vite plugin
-      // that we write.
-      "rust-js-wrapper": `${process.env.buildInputs.split(" ")[0]}/lib/node_modules/js`,
+      ...await gatherBuildInputs(),
     },
   },
   build: {
+    assetsInlineLimit: 0,
     target: "esnext",
     outDir: process.env.out ?? "dist",
   },
