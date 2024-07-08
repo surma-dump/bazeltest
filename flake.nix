aa57c7b6d9299806e1ea6d972d4c02fef4fd56b5
Surma <surma@surma.dev>
2024-07-08 11:39:14 +0100

    As a flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Example Rust flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, fenix, naersk, flake-utils}: 
+    flake-utils.lib.eachDefaultSystem (system:
+      {
+        packages.default = 
+          let
+            target = "wasm32-unknown-unknown";
+            pkgs = import nixpkgs {inherit system;};
+            lib = pkgs.lib;
+            toolchain = with fenix.packages.${system}; combine [
+              stable.rustc
+              stable.cargo
+              targets.${target}.stable.rust-std
+            ];
+            naersk' = pkgs.callPackage naersk {
+              rustc = toolchain;
+              cargo = toolchain;
+            };
+          in 
+          naersk'.buildPackage {
+            name = "lol";
+            src = lib.cleanSource ./.;
+            release = true;
+            copyLibs = true;
+            CARGO_BUILD_TARGET = target;
+          };
+    });
+}
